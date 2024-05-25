c210671fc9850617b1f28018e47d3e1dd69a0f57
edufalcao14 <eduardo.sampaio@student.vinci.be>
2024-05-25 18:27:51 +0200

    exercice : Flutter inputs

---
+++ User_input/lib/views/todo_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ToDoPage extends StatefulWidget {
+  const ToDoPage({super.key});
+
+  @override
+  State<ToDoPage> createState() => _ToDoPageState();
+}
+
+class _ToDoPageState extends State<ToDoPage> {
+
+  //text editing controller to get access to what the user typed
+  TextEditingController myController = TextEditingController();
+
+  //greeting message var
+  String greetingMessage = "";
+
+  //greet user Method
+  void greetUser(){
+    String userName = myController.text;
+    setState(() {
+      greetingMessage = "Hello , " + userName;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(greetingMessage),
+
+              TextField(
+                controller: myController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Type your name...",
+                ),
+              ),
+
+              ElevatedButton(onPressed: greetUser, child: Text("Tap"))
+            ],
+          ),
+        ),
+      ),
+    ) ;
+  }
+}
